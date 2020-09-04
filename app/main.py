f48f8884f57a01210c6e6688dc3f1a4007d75d56
swojak-a <swojak.a@gmail.com>
2020-09-04 11:34:38 +0200

    feat: include posts module and db in main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
-from fastapi import Depends, FastAPI, Header, HTTPException
+from fastapi import FastAPI
 
 from modules.core import api_heath_check
+from modules.posts.routers import router as posts_routers
 
-app = FastAPI()
+from modules.posts import models
+from db import engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(debug=True)
 
 
 app.include_router(api_heath_check.router)
+app.include_router(posts_routers, prefix="/posts", tags=["posts"])
