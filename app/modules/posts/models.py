217c03ec483536b9caa82442edcc5dd5c0eb10eb
swojak-a <swojak.a@gmail.com>
2020-09-26 19:20:56 +0200

    feat: modify Post model to move closer to SQLAlchemy best practices in schema defining

--- app/modules/posts/models.py
+++ app/modules/posts/models.py
@@ -9,6 +9,11 @@ from db import Base
 class Post(Base):
     __tablename__ = "posts"
 
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4, unique=True, nullable=False)
-    title = Column(String, index=True)
-    content = Column(String)
+    id = Column(
+        UUID(as_uuid=True), primary_key=True, default=uuid4, unique=True, nullable=False
+    )
+    title = Column(String(256), index=True, nullable=False)
+    content = Column(String(4096))
+
+    def __repr__(self):
+        return f"<Post={self.id} (title: {self.title[:24] + '...' if len(self.title) > 24 else self.title})>"
