04e64f0755c28ad7e0b6bff0a9f298f88a55ef5e
swojak-a <swojak.a@gmail.com>
2020-09-26 18:44:16 +0200

    feat: add retrieve_post_view and corresponding db_functions in posts module

--- app/modules/posts/db_functions.py
+++ app/modules/posts/db_functions.py
@@ -4,8 +4,12 @@ from .models import Post
 from .schemas import PostSchema
 
 
-def get_posts(db: Session):
+def get_all_posts(db: Session):
     return db.query(Post).all()
+
+
+def get_single_post(db: Session, post_id: int):
+    return db.query(Post).get(post_id)
 
 
 def create_post(db: Session, post: PostSchema):
--- app/modules/posts/routers.py
+++ app/modules/posts/routers.py
@@ -1,11 +1,11 @@
 from typing import TYPE_CHECKING, List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from modules.core.db import get_db
 from .schemas import PostCreateSchema, PostSchema
-from .db_functions import get_posts, create_post
+from .db_functions import get_all_posts, get_single_post, create_post
 
 if TYPE_CHECKING:
     from sqlalchemy.orm import Session  # NOQA
@@ -15,8 +15,17 @@ router = APIRouter()
 
 @router.get("/", response_model=List[PostSchema])
 def list_all_posts_view(db: Session = Depends(get_db)):
-    posts = get_posts(db=db)
+    posts = get_all_posts(db=db)
     return posts
+
+
+@router.get("/{post_id}", response_model=PostSchema)
+def retrieve_post_view(post_id: str, db: Session = Depends(get_db)):
+    post = get_single_post(db=db, post_id=post_id)
+    if post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return post
+
 
 @router.post("/", response_model=PostSchema)
 def create_post_view(post: PostCreateSchema, db: Session = Depends(get_db)):
