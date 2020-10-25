22c488cf5c4a5d895a08951d2af4dca1c1a223b3
swojak-a <swojak.a@gmail.com>
2020-10-25 15:29:33 +0100

    feat: adjust posts modules to recent changes

--- app/modules/posts/schemas.py
+++ app/modules/posts/schemas.py
@@ -1,17 +1,23 @@
+from typing import List
 from uuid import UUID
 
 from pydantic import BaseModel
+
+from modules.comments.schemas import CommentSchema
 
 
 class PostSchemaBase(BaseModel):
     title: str
     content: str
 
+
 class PostCreateSchema(PostSchemaBase):
     pass
 
+
 class PostSchema(PostSchemaBase):
     id: UUID
+    comments: List[CommentSchema] = []
 
     class Config:
         orm_mode = True
