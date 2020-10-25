89f190c658ecf50c12b9d4f2b1f3af8153011c16
swojak-a <swojak.a@gmail.com>
2020-10-25 15:31:13 +0100

    feat: update main to comments creation

--- app/main.py
+++ app/main.py
@@ -2,14 +2,18 @@ from fastapi import FastAPI
 
 from modules.core import api_heath_check
 from modules.posts.routers import router as posts_routers
+from modules.comments.routers import router as comments_router
 
-from modules.posts import models
+import all_models
 from db import engine
 
-models.Base.metadata.create_all(bind=engine)
+modules = ["modules.posts.models"]
+
+all_models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(debug=True)
 
 
 app.include_router(api_heath_check.router)
 app.include_router(posts_routers, prefix="/posts", tags=["posts"])
+app.include_router(comments_router, prefix="/comments", tags=["comments"])
